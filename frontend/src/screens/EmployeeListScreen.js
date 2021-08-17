b4a8caf56a1604275ca9c01e5f341dcb56301608
imimran <engr.aih@gmail.com>
2021-08-17 23:01:22 +0600

    Update EmployeeList

--- frontend/src/screens/EmployeeListScreen.js
+++ frontend/src/screens/EmployeeListScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import Axios from 'axios';
 
 function EmployeeListScreen() {
   const [employees, setEmployee] = useState([]);
@@ -8,25 +8,45 @@ function EmployeeListScreen() {
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(async () => {
-    try {
-      setLoading(true);
-      const data = (await axios.get("/api/v1/all-employee")).data;
-      console.log(data);
-      setEmployee(data);
-      setLoading(false);
-    } catch (error) {
-      setError(true);
-      console.log(error);
-      setLoading(false);
-    }
+
+      Axios.get('/api/v1/all-employee')
+        .then((response) => {    
+                console.log(response.data.data);
+                setEmployee(response.data.data);
+            
+        })
+        .catch((err) => {
+            console.log(err);
+        });
   }, []);
+
   return (
-    <div>
+    <div >
       <h1>Employee</h1>
-      <p>{employees && employees.length}</p>
+      <p>{employees.length}</p>
       {loading ? (<h1>Loading........</h1>) : error ? (<h1>Error</h1> ) : (
         employees.map((employee, index) => {
-          return <h1 key="index">{employee.first_name}</h1>;
+          return (
+            <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First Name</th>
+                <th scope="col">Last Name</th>
+                <th scope="col">Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                  <td>{ employee.first_name}</td>
+                  <td>{ employee.last_name }</td>
+                  <td>{ employee.email }</td>
+              </tr>
+             
+            </tbody>
+          </table>
+          );
         })
       )}
     </div>
