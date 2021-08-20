f4a93eed3e43974b34a4c749ae3af1c79824223f
imimran <engr.aih@gmail.com>
2021-08-21 04:18:10 +0600

    Add HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import EmployeeListScreen from './screens/EmployeeListScreen';
 import AddEmployeeScreen from './screens/AddEmployeeScreen';
 import CSVUploderScreen from './screens/CSVUploderScreen';
 import SendMailScreen from './screens/SendMailScreen';
+import HomeScreen from './screens/HomeScreen';
 function App() {
     return (
         <div className="App">
             <Navbar />
             <Router>
-                <Route path='/employee' exect component={EmployeeListScreen}/>
-                <Route path='/add-employee' exect component={AddEmployeeScreen}/>
-                <Route path='/upload' exect component={CSVUploderScreen}/>
-                <Route path='/send-mail' exect component={SendMailScreen}/>
+            <Switch>
+               
+                <Route path='/employee'  component={EmployeeListScreen} />
+                <Route path='/add-employee'  component={AddEmployeeScreen} />
+                <Route path='/upload'  component={CSVUploderScreen} />
+                <Route path='/send-mail'  component={SendMailScreen} />
+                <Route path='/'  exect component={HomeScreen} />
+                </Switch>
             </Router>
         </div>
     );
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomeScreen() {
+    return (
+        <div className="card card-body p-5 m-5 col-lg-6 mx-auto">
+            Welcome to HR Management
+        </div>
+    )
+}
+
+export default HomeScreen
